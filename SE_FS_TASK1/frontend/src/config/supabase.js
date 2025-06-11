9a966994802d20fd6ce1ffbff455eee1968d5b4b
Partha Sarathi <121345865+Partha-999@users.noreply.github.com>
2025-06-12 01:47:51 +0530

    Update supabase.js

--- SE_FS_TASK1/frontend/src/config/supabase.js
+++ SE_FS_TASK1/frontend/src/config/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://yaqmxfyxdoyouzrswcim.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlhcW14Znl4ZG95b3V6cnN3Y2ltIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk2NTQ3NTQsImV4cCI6MjA2NTIzMDc1NH0.NZylTaAsMdQX1mvrJFD48uSHIpGiXAkgEqxb_BP4r4A';
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
